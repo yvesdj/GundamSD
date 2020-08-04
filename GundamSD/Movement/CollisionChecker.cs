618bdff7bf92e040eaec7eb8409be094e7b23602
yvesdj <yves.dejonghe@student.ap.be>
2020-08-04 14:39:34 +0200

    Added CollisionChecker, CollisionHandler will use Checker for checks

---
+++ GundamSD/Movement/CollisionChecker.cs
@@ -0,0 +1,44 @@
+﻿using GundamSD.Models;
+using Microsoft.Xna.Framework;
+
+namespace GundamSD.Movement
+{
+    public static class CollisionChecker
+    {
+        public static bool IsCollisionBottom(ISprite sprite, Rectangle box)
+        {
+            return sprite.Mover.Velocity.Y > 0 &&
+                    sprite.HitBox.Bottom + sprite.Mover.Velocity.Y > box.Top &&
+                    sprite.HitBox.Top < box.Top &&
+                    sprite.HitBox.Right > box.Left &&
+                    sprite.HitBox.Left < box.Right;
+        }
+
+        public static bool IsCollisionTop(ISprite sprite, Rectangle box)
+        {
+            return sprite.Mover.Velocity.Y < 0 &&
+                    sprite.HitBox.Top + sprite.Mover.Velocity.Y < box.Bottom &&
+                    sprite.HitBox.Bottom > box.Bottom &&
+                    sprite.HitBox.Right > box.Left &&
+                    sprite.HitBox.Left < box.Right;
+        }
+
+        public static bool IsCollisionRight(ISprite sprite, Rectangle box)
+        {
+            return sprite.Mover.Velocity.X > 0 &&
+                    sprite.HitBox.Right + sprite.Mover.Velocity.X > box.Left &&
+                    sprite.HitBox.Left < box.Left &&
+                    sprite.HitBox.Bottom > box.Top &&
+                    sprite.HitBox.Top < box.Bottom;
+        }
+
+        public static bool IsCollisionLeft(ISprite sprite, Rectangle box)
+        {
+            return sprite.Mover.Velocity.X < 0 &&
+                    sprite.HitBox.Left + sprite.Mover.Velocity.X < box.Right &&
+                    sprite.HitBox.Right > box.Right &&
+                    sprite.HitBox.Bottom > box.Top &&
+                    sprite.HitBox.Top < box.Bottom;
+        }
+    }
+}
--- GundamSD/Movement/CollisionHandler.cs
+++ GundamSD/Movement/CollisionHandler.cs
@@ -26,11 +26,7 @@ namespace GundamSD.Movement
             List<Rectangle> collisionBoxes = mapManager.GetMapCollidables();
             foreach (Rectangle box in collisionBoxes)
             {
-                if (_sprite.Mover.Velocity.Y > 0 &&
-                    _sprite.HitBox.Bottom + _sprite.Mover.Velocity.Y > box.Top &&
-                    _sprite.HitBox.Top < box.Top &&
-                    _sprite.HitBox.Right > box.Left &&
-                    _sprite.HitBox.Left < box.Right)
+                if (CollisionChecker.IsCollisionBottom(_sprite, box))
                 {
                     //_sprite.Mover.NextPosition = new Vector2(_sprite.Mover.NextPosition.X, _sprite.Mover.NextPosition.Y - _sprite.Speed);
                     _sprite.Mover.Velocity = new Vector2(_sprite.Mover.Velocity.X, 0);
@@ -38,33 +34,21 @@ namespace GundamSD.Movement
                     IsGrounded = true;
 
                 }
-                else if (_sprite.Mover.Velocity.Y < 0 &&
-                    _sprite.HitBox.Top + _sprite.Mover.Velocity.Y < box.Bottom &&
-                    _sprite.HitBox.Bottom > box.Bottom &&
-                    _sprite.HitBox.Right > box.Left &&
-                    _sprite.HitBox.Left < box.Right)
+                else if (CollisionChecker.IsCollisionTop(_sprite, box))
                 {
                     //_sprite.Mover.NextPosition = new Vector2(_sprite.Mover.NextPosition.X, _sprite.Mover.NextPosition.Y + _sprite.Speed);
                     _sprite.Mover.Velocity = new Vector2(_sprite.Mover.Velocity.X, 0);
                     IsColliding = true;
                 }
 
-                else if (_sprite.Mover.Velocity.X > 0 &&
-                    _sprite.HitBox.Right + _sprite.Mover.Velocity.X > box.Left &&
-                    _sprite.HitBox.Left < box.Left &&
-                    _sprite.HitBox.Bottom > box.Top &&
-                    _sprite.HitBox.Top < box.Bottom)
+                else if (CollisionChecker.IsCollisionRight(_sprite, box))
                 {
                     //_sprite.Mover.NextPosition = new Vector2(_sprite.Mover.NextPosition.X - _sprite.Speed, _sprite.Mover.NextPosition.Y);
                     _sprite.Mover.Velocity = new Vector2(0, _sprite.Mover.Velocity.Y);
                     IsColliding = true;
 
                 }
-                else if (_sprite.Mover.Velocity.X < 0 &&
-                    _sprite.HitBox.Left + _sprite.Mover.Velocity.X < box.Right &&
-                    _sprite.HitBox.Right > box.Right &&
-                    _sprite.HitBox.Bottom > box.Top &&
-                    _sprite.HitBox.Top < box.Bottom)
+                else if (CollisionChecker.IsCollisionLeft(_sprite, box))
                 {
                     //_sprite.Mover.NextPosition = new Vector2(_sprite.Mover.NextPosition.X + _sprite.Speed, _sprite.Mover.NextPosition.Y);
                     _sprite.Mover.Velocity = new Vector2(0, _sprite.Mover.Velocity.Y);
@@ -77,6 +61,7 @@ namespace GundamSD.Movement
             }
         }
 
+        //not sure if sprites should be able to pass eachother, or block path
         public void CheckCollisionSprite(MapManager mapManager)
         {
             List<ISprite> otherSprites = mapManager.Sprites;

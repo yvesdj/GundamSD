1c6e475ff870658bd63d0d684632b02aa1989596
yvesdj <yves.dejonghe@student.ap.be>
2020-08-25 20:38:31 +0200

    Started making a state management system, restructured project

---
+++ GundamSD/Models/Enemies/GruntRanged.cs
@@ -0,0 +1,37 @@
+﻿using GundamSD.Animations;
+using GundamSD.Maps;
+using GundamSD.Movement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GundamSD.Models
+{
+    public class GruntRanged : GruntBase
+    {
+        public IWeapon RangedWeapon { get; set; }
+
+        public GruntRanged(Texture2D atlasTexture) : base(atlasTexture)
+        {
+            IAnimationAtlasAction RangedLeft = Factory.CreateAnimAtlasAction(11, 11, true);
+            IAnimationAtlasAction RangedRight = Factory.CreateAnimAtlasAction(12, 12, true);
+
+            Actions.Add("RangedLeft", RangedLeft);
+            Actions.Add("RangedRight", RangedRight);
+
+            Mover = new MoverAI(this, true);
+            RangedWeapon = new RangedWeaponAI(this, 10, 480, 500f);
+
+        }
+
+        public override void Update(GameTime gameTime, MapManager mapManager)
+        {
+            base.Update(gameTime, mapManager);
+            RangedWeapon.DealDamage(mapManager, gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
---
+++ GundamSD/StateManagement/GameState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GundamSD.StateManagement
+{
+    public abstract class GameState : IGameState
+    {
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
